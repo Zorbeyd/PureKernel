cd34cc95afa1aa85b965ae7a08fd1ad130070110
Zorbey <zorbeyd@gmail.com>
2017-02-03 23:08:17 +0300

    BePermissive

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -220,12 +220,11 @@ out:
 	return length;
 }
 #else
-#define sel_write_enforce NULL
 #endif
 
 static const struct file_operations sel_enforce_ops = {
 	.read		= sel_read_enforce,
-	.write		= sel_write_enforce,
+	.write		= NULL,
 	.llseek		= generic_file_llseek,
 };
 
